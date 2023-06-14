65bd38c0817ba11837d4a23a9a21789767953578
Nguyen Duc Vuong <32158927+whatever1211@users.noreply.github.com>
2023-06-14 19:30:32 +0700

    Create implementation.py

---
+++ algorithms/implementation.py
@@ -0,0 +1,41 @@
+# 1. Grading Students
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'gradingStudents' function below.
+#
+# The function is expected to return an INTEGER_ARRAY.
+# The function accepts INTEGER_ARRAY grades as parameter.
+#
+
+def gradingStudents(grades):
+    # Write your code here
+    for index in range(len(grades)):
+        if grades[index] >= 38 and grades[index] % 5 > 2:
+            grades[index] += 5 - grades[index] % 5
+    return grades
+            
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    grades_count = int(input().strip())
+
+    grades = []
+
+    for _ in range(grades_count):
+        grades_item = int(input().strip())
+        grades.append(grades_item)
+
+    result = gradingStudents(grades)
+
+    fptr.write('\n'.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
