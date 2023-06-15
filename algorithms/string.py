5380295afd86c04a88cbda0d7d95ba7c740c1c06
Nguyen Duc Vuong <32158927+whatever1211@users.noreply.github.com>
2023-06-15 23:19:08 +0700

    Update string.py

--- algorithms/string.py
+++ algorithms/string.py
@@ -1,5 +1,5 @@
 # -------------------------------------------------------------------------------------------------------------------
-# 1. Strong Password 
+# 1. Strong Password
 #!/bin/python3
 
 import math
@@ -54,5 +54,83 @@ if __name__ == '__main__':
     answer = minimumNumber(n, password)
 
     fptr.write(str(answer) + '\n')
+
+    fptr.close()
+
+# -------------------------------------------------------------------------------------------------------------------
+# 2. Highest Value Palindrome
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'highestValuePalindrome' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING s
+#  2. INTEGER n
+#  3. INTEGER k
+#
+
+def highestValuePalindrome(s, n, k):
+    # Write your code here
+    sArr = [char for char in s]
+    minimum_change_require = 0
+    for i in range(math.floor(n / 2)):
+        if sArr[i] != sArr[n - 1 - i]:
+            minimum_change_require += 1
+    if minimum_change_require > k:
+        return "-1"
+    i = 0
+    print (i, k, minimum_change_require)
+    while i < math.floor(n / 2):
+        if sArr[i] == sArr[n - 1 - i]:
+            if k - minimum_change_require > 1 and sArr[i] != "9":
+                sArr[i] = "9"
+                sArr[n - 1 - i] = "9"
+                k -= 2
+        else:
+            if k - minimum_change_require > 0:
+                minusK = 0
+                if sArr[i] != "9":
+                    minusK += 1
+                    sArr[i] = "9"
+                if sArr[n - 1 - i] != "9":
+                    minusK += 1
+                    sArr[n - 1 - i] = "9"
+                k -= minusK
+                minimum_change_require -= 1
+            else:
+                maxS = str(max(int(sArr[i]), int(sArr[n - 1 - i])))
+                sArr[i] = maxS
+                sArr[n - 1 - i] = maxS
+                k -= 1
+                minimum_change_require -= 1
+        i += 1
+    print (i, k, minimum_change_require)
+    if n % 2 == 1 and sArr[i] != "9":
+        if k - minimum_change_require > 0:
+            sArr[i] = "9"
+    return "".join(sArr)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    s = input()
+
+    result = highestValuePalindrome(s, n, k)
+
+    fptr.write(result + '\n')
 
     fptr.close()
