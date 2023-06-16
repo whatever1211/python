05730713016ca828c6a4de28cf2c9d1079bf6eef
Nguyen Duc Vuong <32158927+whatever1211@users.noreply.github.com>
2023-06-16 21:12:21 +0700

    Update recursion.py

--- algorithms/recursion.py
+++ algorithms/recursion.py
@@ -42,3 +42,62 @@ if __name__ == '__main__':
     fptr.write(str(result) + '\n')
 
     fptr.close()
+
+# -------------------------------------------------------------------------------------------------------------------
+# 2. Password Cracker
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'passwordCracker' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. STRING_ARRAY passwords
+#  2. STRING loginAttempt
+#
+
+def passwordCracker(passwords, loginAttempt):
+    # Write your code here
+    result = []    
+    for passwd in passwords:
+        if passwd == loginAttempt[:len(passwd)]:
+            if len(passwd) == len(loginAttempt):                
+                result.append(passwd)
+                break
+            else:
+                passwdArr = passwordCracker(passwords, loginAttempt[len(passwd):])
+                if passwdArr:
+                    passwdArr.append(passwd)
+                    result = passwdArr
+                    break
+    return result
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    t = int(input().strip())
+
+    for t_itr in range(t):
+        n = int(input().strip())
+
+        passwords = input().rstrip().split()
+
+        loginAttempt = input()
+
+        result = passwordCracker(passwords, loginAttempt)
+        
+        if result:
+            result.reverse()
+            result = " ".join(result)
+        else:
+            result = "WRONG PASSWORD"
+
+        fptr.write(result + '\n')
+
+    fptr.close()
