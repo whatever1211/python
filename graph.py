09887fb7635161b3539b04c064a425c1fb5a3d6d
Nguyen Duc Vuong <32158927+whatever1211@users.noreply.github.com>
2023-06-18 17:17:00 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -464,3 +464,257 @@ if __name__ == '__main__':
 
 # This code is contributed by Neelam Yadav
 # Improved by James Graça-Jones
+
+# -------------------------------------------------------------------------------------------------------------------
+# Eulerian check
+# Python program to check if a given graph is Eulerian or not
+#Complexity : O(V+E)
+
+from collections import defaultdict
+
+# This class represents a undirected graph using adjacency list representation
+
+
+class Graph:
+
+	def __init__(self, vertices):
+		self.V = vertices # No. of vertices
+		self.graph = defaultdict(list) # default dictionary to store graph
+
+	# function to add an edge to graph
+	def addEdge(self, u, v):
+		self.graph[u].append(v)
+		self.graph[v].append(u)
+
+	# A function used by isConnected
+	def DFSUtil(self, v, visited):
+		# Mark the current node as visited
+		visited[v] = True
+
+		# Recur for all the vertices adjacent to this vertex
+		for i in self.graph[v]:
+			if visited[i] == False:
+				self.DFSUtil(i, visited)
+
+	'''Method to check if all non-zero degree vertices are
+	connected. It mainly does DFS traversal starting from
+	node with non-zero degree'''
+
+	def isConnected(self):
+
+		# Mark all the vertices as not visited
+		visited = [False]*(self.V)
+
+		# Find a vertex with non-zero degree
+		for i in range(self.V):
+			if len(self.graph[i]) != 0:
+				break
+
+		# If there are no edges in the graph, return true
+		if i == self.V-1:
+			return True
+
+		# Start DFS traversal from a vertex with non-zero degree
+		self.DFSUtil(i, visited)
+
+		# Check if all non-zero degree vertices are visited
+		for i in range(self.V):
+			if visited[i] == False and len(self.graph[i]) > 0:
+				return False
+
+		return True
+
+	'''The function returns one of the following values
+	0 --> If graph is not Eulerian
+	1 --> If graph has an Euler path (Semi-Eulerian)
+	2 --> If graph has an Euler Circuit (Eulerian) '''
+
+	def isEulerian(self):
+		# Check if all non-zero degree vertices are connected
+		if self.isConnected() == False:
+			return 0
+		else:
+			# Count vertices with odd degree
+			odd = 0
+			for i in range(self.V):
+				if len(self.graph[i]) % 2 != 0:
+					odd += 1
+
+			'''If odd count is 2, then semi-eulerian.
+			If odd count is 0, then eulerian
+			If count is more than 2, then graph is not Eulerian
+			Note that odd count can never be 1 for undirected graph'''
+			if odd == 0:
+				return 2
+			elif odd == 2:
+				return 1
+			elif odd > 2:
+				return 0
+
+	# Function to run test cases
+
+	def test(self):
+		res = self.isEulerian()
+		if res == 0:
+			print("graph is not Eulerian")
+		elif res == 1:
+			print("graph has a Euler path")
+		else:
+			print("graph has a Euler cycle")
+
+
+# Let us create and test graphs shown in above figures
+g1 = Graph(5)
+g1.addEdge(1, 0)
+g1.addEdge(0, 2)
+g1.addEdge(2, 1)
+g1.addEdge(0, 3)
+g1.addEdge(3, 4)
+g1.test()
+
+g2 = Graph(5)
+g2.addEdge(1, 0)
+g2.addEdge(0, 2)
+g2.addEdge(2, 1)
+g2.addEdge(0, 3)
+g2.addEdge(3, 4)
+g2.addEdge(4, 0)
+g2.test()
+
+g3 = Graph(5)
+g3.addEdge(1, 0)
+g3.addEdge(0, 2)
+g3.addEdge(2, 1)
+g3.addEdge(0, 3)
+g3.addEdge(3, 4)
+g3.addEdge(1, 3)
+g3.test()
+
+# Let us create a graph with 3 vertices
+# connected in the form of cycle
+g4 = Graph(3)
+g4.addEdge(0, 1)
+g4.addEdge(1, 2)
+g4.addEdge(2, 0)
+g4.test()
+
+# Let us create a graph with all vertices
+# with zero degree
+g5 = Graph(3)
+g5.test()
+
+# This code is contributed by Neelam Yadav
+
+# -------------------------------------------------------------------------------------------------------------------
+# Hamiltonian
+# Python program for solution of
+# hamiltonian cycle problem
+
+class Graph():
+	def __init__(self, vertices):
+		self.graph = [[0 for column in range(vertices)]
+							for row in range(vertices)]
+		self.V = vertices
+
+	''' Check if this vertex is an adjacent vertex
+		of the previously added vertex and is not
+		included in the path earlier '''
+	def isSafe(self, v, pos, path):
+		# Check if current vertex and last vertex
+		# in path are adjacent
+		if self.graph[ path[pos-1] ][v] == 0:
+			return False
+
+		# Check if current vertex not already in path
+		for vertex in path:
+			if vertex == v:
+				return False
+
+		return True
+
+	# A recursive utility function to solve
+	# hamiltonian cycle problem
+	def hamCycleUtil(self, path, pos):
+
+		# base case: if all vertices are
+		# included in the path
+		if pos == self.V:
+			# Last vertex must be adjacent to the
+			# first vertex in path to make a cycle
+			if self.graph[ path[pos-1] ][ path[0] ] == 1:
+				return True
+			else:
+				return False
+
+		# Try different vertices as a next candidate
+		# in Hamiltonian Cycle. We don't try for 0 as
+		# we included 0 as starting point in hamCycle()
+		for v in range(1,self.V):
+
+			if self.isSafe(v, pos, path) == True:
+
+				path[pos] = v
+
+				if self.hamCycleUtil(path, pos+1) == True:
+					return True
+
+				# Remove current vertex if it doesn't
+				# lead to a solution
+				path[pos] = -1
+
+		return False
+
+	def hamCycle(self):
+		path = [-1] * self.V
+
+		''' Let us put vertex 0 as the first vertex
+			in the path. If there is a Hamiltonian Cycle,
+			then the path can be started from any point
+			of the cycle as the graph is undirected '''
+		path[0] = 0
+
+		if self.hamCycleUtil(path,1) == False:
+			print ("Solution does not exist\n")
+			return False
+
+		self.printSolution(path)
+		return True
+
+	def printSolution(self, path):
+		print ("Solution Exists: Following",
+				"is one Hamiltonian Cycle")
+		for vertex in path:
+			print (vertex, end = " ")
+		print (path[0], "\n")
+
+# Driver Code
+
+''' Let us create the following graph
+	(0)--(1)--(2)
+	| / \ |
+	| / \ |
+	| /	 \ |
+	(3)-------(4) '''
+g1 = Graph(5)
+g1.graph = [ [0, 1, 0, 1, 0], [1, 0, 1, 1, 1],
+			[0, 1, 0, 0, 1,],[1, 1, 0, 0, 1],
+			[0, 1, 1, 1, 0], ]
+
+# Print the solution
+g1.hamCycle();
+
+''' Let us create the following graph
+	(0)--(1)--(2)
+	| / \ |
+	| / \ |
+	| /	 \ |
+	(3)	 (4) '''
+g2 = Graph(5)
+g2.graph = [ [0, 1, 0, 1, 0], [1, 0, 1, 1, 1],
+		[0, 1, 0, 0, 1,], [1, 1, 0, 0, 0],
+		[0, 1, 1, 0, 0], ]
+
+# Print the solution
+g2.hamCycle();
+
+# This code is contributed by Divyanshu Mehta
