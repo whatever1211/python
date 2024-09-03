fcfd8b12b2cf56abab57fbc8bc55116344b7b109
Nguyen Duc Vuong <32158927+whatever1211@users.noreply.github.com>
2024-09-03 11:00:58 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -725,6 +725,14 @@ g3.addEdge(2, 4)
 g3.printEulerTour()
 
 #This code is contributed by Neelam Yadav
+'''
+Properties
+An undirected graph has an Eulerian cycle if and only if every vertex has even degree, and all of its vertices with nonzero degree belong to a single connected component.[6]
+An undirected graph can be decomposed into edge-disjoint cycles if and only if all of its vertices have even degree. So, a graph has an Eulerian cycle if and only if it can be decomposed into edge-disjoint cycles and its nonzero-degree vertices belong to a single connected component.
+An undirected graph has an Eulerian trail if and only if exactly zero or two vertices have odd degree, and all of its vertices with nonzero degree belong to a single connected component[6]
+A directed graph has an Eulerian cycle if and only if every vertex has equal in degree and out degree, and all of its vertices with nonzero degree belong to a single strongly connected component. Equivalently, a directed graph has an Eulerian cycle if and only if it can be decomposed into edge-disjoint directed cycles and all of its vertices with nonzero degree belong to a single strongly connected component.[6]
+A directed graph has an Eulerian trail if and only if at most one vertex has (out-degree) − (in-degree) = 1, at most one vertex has (in-degree) − (out-degree) = 1, every other vertex has equal in-degree and out-degree, and all of its vertices with nonzero degree belong to a single connected component of the underlying undirected graph.[6]
+'''
 
 # -------------------------------------------------------------------------------------------------------------------
 # Hamiltonian Cycle
