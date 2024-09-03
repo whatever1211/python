70a151b5fffccf5d739371a844bf70864e3a02d2
Nguyen Duc Vuong <32158927+whatever1211@users.noreply.github.com>
2024-09-03 10:49:56 +0700

    Update graph.py

--- graph.py
+++ graph.py
@@ -1155,3 +1155,60 @@ print ("\nBridges in third graph ")
 g3.bridge()
 
 #This code is contributed by Neelam Yadav
+
+# -------------------------------------------------------------------------------------------------------------------
+class TarjanSCC:
+    def __init__(self, graph):
+        self.graph = graph
+        self.time = 0
+        self.stack = []
+        self.low_link = {}
+        self.visited = set()
+        self.sccs = []
+
+    def _dfs(self, v):
+        self.low_link[v] = self.time
+        cur_time = self.time
+        self.time += 1
+        self.stack.append(v)
+        self.visited.add(v)
+
+        for neighbor in self.graph[v]:
+            if neighbor not in self.low_link:
+                self._dfs(neighbor)
+                self.low_link[v] = min(self.low_link[v], self.low_link[neighbor])
+            elif neighbor in self.stack:
+                self.low_link[v] = min(self.low_link[v], self.low_link[neighbor])
+
+        if self.low_link[v] == cur_time:
+            scc = []
+            while True:
+                u = self.stack.pop()
+                scc.append(u)
+                if u == v:
+                    break
+            self.sccs.append(scc)
+
+    def find_sccs(self):
+        for v in self.graph:
+            if v not in self.low_link:
+                self._dfs(v)
+        return self.sccs
+
+# Example usage:
+graph = {
+    0: [1],
+    1: [2, 4, 5],
+    2: [3, 6],
+    3: [2, 7],
+    4: [0, 5],
+    5: [6],
+    6: [5],
+    7: [3, 6]
+}
+
+tarjan = TarjanSCC(graph)
+sccs = tarjan.find_sccs()
+print("Strongly Connected Components:")
+for scc in sccs:
+    print(scc)
